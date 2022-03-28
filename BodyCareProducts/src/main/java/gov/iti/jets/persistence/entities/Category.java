fc83e901875685ad22943b393a58c3f8c24a4706
HendElAhmadi <1998hend@gmail.com>
2022-03-29 00:21:03 +0200

    layerPackages

---
+++ BodyCareProducts/src/main/java/gov/iti/jets/persistence/entities/Category.java
@@ -0,0 +1,54 @@
+package gov.iti.jets.persistence.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="category_id",length = 10)
+    private int id;
+
+    @Column(name="category_name",length = 25)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
+    
+
