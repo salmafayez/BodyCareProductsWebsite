d0293cc8200cac842591691ca5066189c8148c5d
salmaFayez <55198618+salmafayez@users.noreply.github.com>
2022-04-15 15:30:25 +0200

    update header

--- BodyCareProducts/src/main/webapp/js/wishlist.js
+++ BodyCareProducts/src/main/webapp/js/wishlist.js
@@ -1,7 +1,7 @@
 
 function addToWishlist(id){
     $.ajax({
-        url: 'wishlist?t='+new Date().getTime(),
+        url: 'wishlist-add?t='+new Date().getTime(),
         type: 'POST',
         data: jQuery.param({ productId: id}) ,
         contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
